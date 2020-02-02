ec071b8d3dab94d102cbd4af7b10878b283319bb
takatoshiH <t.hikijiri@gmail.com>
2020-02-03 01:14:00 +0900

    頑張った

--- Hello/hello.js
+++ Hello/hello.js
@@ -1,11 +1,17 @@
 var app = new Vue({  //Vueクラスをインスタンス化している
   el: "#app",  //Vue.jsを適用する要素
   data: {　//データオブジェクト　リアクティブデータと呼ぶ　リアクティブデータを管理する仕組みをリアクティブシステムと言う
-    email: "Good@gmail.com",
+    current: new Date(),
   },
-  methods: {
-    localEmail: function () {
-      return this.email.split("@")[0].toLowerCase();
-    }
+  created: function () {　//createするときにタイマーをセットする
+    let that = this;
+    this.timer = setInterval(function () {
+      that.current = new Date(); //１秒ごとに時間を更新する
+    }, 1000);
+  },
+  beforeDestory: function () {
+    clearInterval(this.timer);
   }
 });
+
+//dataオプションにデータを登録すると、その全てのプロパティを監視対象として登録する
