f937e973aae4ba9a21a7153567272c436e125376
takatoshiH <t.hikijiri@gmail.com>
2020-02-02 23:39:12 +0900

    細かい修正uusei

--- Hello/hello.js
+++ Hello/hello.js
@@ -1,6 +1,6 @@
 var app = new Vue({  //Vueクラスをインスタンス化している
   el: "#app",  //Vue.jsを適用する要素
-  data: {　//データオブジェクト
+  data: {　//データオブジェクト　リアクティブデータと呼ぶ　リアクティブデータを管理する仕組みをリアクティブシステムと言う
     email: "Good@gmail.com",
   },
   methods: {
--- Todo/main.js
+++ Todo/main.js
@@ -5,19 +5,7 @@
     el: "#app",
     data: {
       newItem: "",
-      todos: [{
-        title: "task 1",
-        isDone: false
-      },
-      {
-        title: "task 2",
-        isDone: false
-        },
-        {
-          title: "task 3",
-          isDone: true
-        },
-      ]
+      todos: []
     },
     watch: {
       todos: {
